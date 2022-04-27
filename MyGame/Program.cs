7838bcead8f6d1ee3ea92abef84e7df3d9e21b50
Pedroosorio97 <pedromrnosorio97@gmail.com>
2022-04-27 15:23:47 +0100

    Program correctly asks player for enemy number, then name per enemy and shows the names

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -6,7 +6,30 @@ namespace MyGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("How many Enemies you want: ");
+            int numberOfEnemies = int.Parse(Console.ReadLine());
+            EnemyCreator(numberOfEnemies);
+            
+        }
+
+        public static void EnemyCreator(int enemyNumber)
+        {
+            Enemy[] enemiesArray = new Enemy[enemyNumber];
+            int i = 0;
+            foreach (Enemy enemy in enemiesArray)
+            {
+                Console.WriteLine("Write Enemy Name: ");
+                string enemyName = Console.ReadLine();
+                Enemy enemyInstance = new Enemy(enemyName);
+                enemiesArray[i] = enemyInstance;
+                i++;
+            }
+            for (int j = 0; j < enemiesArray.Length; j++)
+            {
+                Console.WriteLine(enemiesArray[j].GetName());
+                    
+            }
+            
         }
     }
 }
